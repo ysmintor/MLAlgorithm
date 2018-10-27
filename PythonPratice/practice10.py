bf6be4f00db8a7b64408b46f76dbf2436432a800
YorkYu <ysmintor@gmail.com>
2018-10-27 23:52:12 +0800

    add practice10

---
+++ PythonPratice/practice10.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+print(sorted([36, 5, -12, 9, -21]))
+print(sorted([36, 5, -12, 9, -21], key=abs))
+
+print(sorted(['bob', 'about', 'Zoo', 'Credit']))
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower))
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True))
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+def by_name(t):
+    return t[0].lower()
+
+
+L2 = sorted(L, key=by_name)
+print(L2)
