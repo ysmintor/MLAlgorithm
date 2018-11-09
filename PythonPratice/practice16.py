6e7f5a37b5648667e3f8e6e0d0a6665fc40fba3c
YorkYu <ysmintor@gmail.com>
2018-11-09 23:53:46 +0800

    use __slots__

--- PythonPratice/practice16.py
+++ PythonPratice/practice16.py
@@ -2,3 +2,16 @@ type(123)
 print(type(123))
 
 print(dir('ABC'))
+
+class Student(object):
+    pass
+
+s = Student()
+s.name = 'Michael'
+print(s.name)
+
+def set_age(self, age):
+    self.age = age
+
+from types import MethodType
+s.set_age = MethodType(set_age, s)
