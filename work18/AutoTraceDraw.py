5089a9e0ad96117757227e22972be39fdc2a4401
YorkYu <ysmintor@gmail.com>
2019-03-16 15:54:03 +0800

    PEP8 rules

--- work18/AutoTraceDraw.py
+++ work18/AutoTraceDraw.py
@@ -1,17 +1,17 @@
-#AutoTraceDraw.py
+# AutoTraceDraw.py
 import turtle as t
 t.title('自动轨迹绘制')
 t.setup(800, 600, 0, 0)
 t.pencolor("red")
 t.pensize(5)
-#数据读取
+# 数据读取
 datals=[]
 f = open("data.txt")
 for line in f:
     line = line.replace("\n", "")
     datals.append(list(map(eval, line.split(","))))
 f.close()
-#自动绘制
+# 自动绘制
 for i in range(len(datals)):
     t.pencolor(datals[i][3], datals[i][4], datals[i][5])
     t.fd(datals[i][0])
