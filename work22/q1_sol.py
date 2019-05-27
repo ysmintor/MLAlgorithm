005ca9b0d314eca5998d9c8110154462ba898d1a
YorkYu <ysmintor@gmail.com>
2019-05-27 12:58:01 +0800

    modify answer

--- work22/q1_sol.py
+++ work22/q1_sol.py
@@ -1,5 +1,5 @@
 """
-Simple exercises to get used to TensorFlow API
+Solution to simple exercises to get used to TensorFlow API
 You should thoroughly test your code.
 TensorFlow's official documentation should be your best friend here
 CS20: "TensorFlow for Deep Learning Research"
@@ -22,7 +22,6 @@ sess = tf.InteractiveSession()
 x = tf.random_uniform([])  # Empty array as shape creates a scalar.
 y = tf.random_uniform([])
 out = tf.cond(tf.greater(x, y), lambda: tf.add(x, y), lambda: tf.subtract(x, y))
-print(sess.run(out))
 
 ###############################################################################
 # 1b: Create two 0-d tensors x and y randomly selected from the range [-1, 1).
@@ -33,9 +32,10 @@ print(sess.run(out))
 x = tf.random_uniform([], -1, 1, dtype=tf.float32)
 y = tf.random_uniform([], -1, 1, dtype=tf.float32)
 out = tf.case({tf.less(x, y): lambda: tf.add(x, y),
-               tf.greater(x, y): lambda : tf.subtract(x, y)},
-              default= lambda : tf.constant(0.0), exclusive=True)
-print(sess.run(out))
+			tf.greater(x, y): lambda: tf.subtract(x, y)},
+			default=lambda: tf.constant(0.0), exclusive=True)
+
+
 ###############################################################################
 # 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]]
 # and y as a tensor of zeros with the same shape as x.
@@ -46,7 +46,6 @@ print(sess.run(out))
 x = tf.constant([[0, -2, -1], [0, 1, 2]])
 y = tf.zeros_like(x)
 out = tf.equal(x, y)
-print(sess.run(out))
 
 ###############################################################################
 # 1d: Create the tensor x of value
@@ -62,14 +61,12 @@ print(sess.run(out))
 ###############################################################################
 
 x = tf.constant([29.05088806,  27.61298943,  31.19073486,  29.35532951,
-  30.97266006,  26.67541885,  38.08450317,  20.74983215,
-  34.94445419,  34.45999146,  29.06485367,  36.01657104,
-  27.88236427,  20.56035233,  30.20379066,  29.51215172,
-  33.71149445,  28.59134293,  36.05556488,  28.66994858])
-
+		        30.97266006,  26.67541885,  38.08450317,  20.74983215,
+		        34.94445419,  34.45999146,  29.06485367,  36.01657104,
+		        27.88236427,  20.56035233,  30.20379066,  29.51215172,
+		        33.71149445,  28.59134293,  36.05556488,  28.66994858])
 indices = tf.where(x > 30)
 out = tf.gather(x, indices)
-print(sess.run(out))
 
 ###############################################################################
 # 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
@@ -77,8 +74,8 @@ print(sess.run(out))
 # Hint: Use tf.range() and tf.diag().
 ###############################################################################
 
-out = tf.diag(tf.range(1, 7))
-print(sess.run(out))
+values = tf.range(1, 7)
+out = tf.diag(values)
 
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
@@ -88,7 +85,6 @@ print(sess.run(out))
 
 m = tf.random_normal([10, 10], mean=10, stddev=1)
 out = tf.matrix_determinant(m)
-print(sess.run(out))
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -98,7 +94,6 @@ print(sess.run(out))
 
 x = tf.constant([5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
 unique_values, indices = tf.unique(x)
-print(sess.run(unique_values))
 
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
@@ -113,6 +108,6 @@ print(sess.run(unique_values))
 x = tf.random_normal([300], mean=5, stddev=1)
 y = tf.random_normal([300], mean=5, stddev=1)
 average = tf.reduce_mean(x - y)
-def f1(): return tf.reduce_mean(tf.square(x - y ))
+def f1(): return tf.reduce_mean(tf.square(x - y))
 def f2(): return tf.reduce_sum(tf.abs(x - y))
-tf.cond(average < 0, f1, f2)
+out = tf.cond(average < 0, f1, f2)
